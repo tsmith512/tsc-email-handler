0946441720d8d16945bd99e6df9d12b90b1ab07d
Taylor Smith <tsmith@tsmithcreative.com>
2021-08-05 11:34:02 -0500

    Frame out the new email handler with aws4fetch

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+import { AwsClient } from 'aws4fetch';
+
+const aws = new AwsClient({ accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY });
+const SES_ENDPOINT = 'https://email.us-west-2.amazonaws.com/';
+
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
+  // See tsmith512/tsmithcreative:_js/contact.js
+  const messagePayload = await readRequestBody(request);
+
+  // Reformulate the payload for SES
+
+  // Sign it
+
+  // Send it
+
+  // Return a 200 or an error (@TODO: And make the frontend handle an error...)
+
   return new Response('Hello worker!', {
     headers: { 'content-type': 'text/plain' },
-  })
+  });
 }
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+})
