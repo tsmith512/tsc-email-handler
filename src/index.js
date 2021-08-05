b353805e48864d48028cc524a0b7b46ab80a1769
Taylor Smith <tsmith@tsmithcreative.com>
2021-08-05 14:27:01 -0500

    Clean up and simplify

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
 import { AwsClient } from 'aws4fetch';
 
-const aws = new AwsClient({ accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY });
+const aws = new AwsClient({
+  accessKeyId: AWS_ACCESS_KEY,
+  secretAccessKey: AWS_SECRET_KEY
+});
+
 const SES_ENDPOINT = 'https://email.us-east-1.amazonaws.com/';
 
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
 async function handleRequest(request) {
   // See tsmith512/tsmithcreative:_js/contact.js
-  const messagePayload = await request.json();
+  const { from = "", replyto = FROM_ADDRESS, message = "" } = await request.json();
 
-  // Reformulate the payload for SES
+  // @TODO: [Insert some validations here one day...]
+
+  // Build the email "object" (str) for SES.
   const messageRaw =
     `From: ${FROM_ADDRESS}
     To: ${FROM_ADDRESS}
-    Reply-To: ${messagePayload.replyto}
-    Subject: Website Referral Form from ${messagePayload.from}
+    Reply-To: ${replyto}
+    Subject: Website Referral Form from ${from}
 
-    ${messagePayload.message}
+    ${message}
   `.replace(/\n[ ]+/g, '\n');
 
   const sesPayload = `Action=SendRawEmail&Destinations.member.1=${encodeURIComponent(FROM_ADDRESS)}&RawMessage.Data=${encodeURIComponent(btoa(messageRaw))}`
 
-  // Sign it
+  // Build signed request with aws4fetch
   const signedRequest = await aws.sign(SES_ENDPOINT, {
     method: 'POST',
     headers: {
@@ -32,19 +34,23 @@ async function handleRequest(request) {
     body: sesPayload,
   });
 
-  console.log(JSON.stringify(signedRequest));
-
-  // Send it
+  // Send the SES request
   const sesResponse = await fetch(signedRequest);
 
-  const responseInfo = await sesResponse.text()
-  console.log(responseInfo);
+  // Return a simplified response to frontend
+  const { ok, status } = await sesResponse;
 
-  // Return a 200 or an error (@TODO: And make the frontend handle an error...)
-
-  return new Response(responseInfo, {
-    headers: { 'content-type': 'text/plain' },
-  });
+  if (ok) {
+    return new Response(`SES responded okay ${status}`, {
+      status: 200,
+      headers: { 'content-type': 'text/plain' },
+    });
+  }
+  else {
+    return new Response(`SES responded with error ${status}`, {
+      status: 500,
+    });
+  }
 }
 
 addEventListener('fetch', event => {
